c481d2f98fbc643dbb3bb5289694734a7b44b80a
siva <desinidisivagangadhar@gmail.com>
2023-11-21 12:37:50 +0530

    first commit to github repo

--- shellscript1.sh
+++ shellscript1.sh
@@ -4,3 +4,8 @@ echo "HelloWorld"
     person2=sathish
 echo "$PERSON1:Hi GOOD MORNING $PERSON2"
 echo "$PERSON2:Hi GOOD MORNING $PERSON1"
+echo "HIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII"
+echo "we will win"
+echo "hello"
+
+
